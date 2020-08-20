737fb978e2458feb7bd2aeb63e2c8eb2c273258e
S S S <48981600+S-S005500@users.noreply.github.com>
2020-08-20 13:42:57 +0000

    Initialize OTP_USB_IDs

---
+++ firmware/common/rom_iap.c
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2013 Benjamin Vernoux <titanmkd@gmail.com>
+ *
+ * This file is part of HackRF.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+ #include "hackrf_core.h"
+#include <stdint.h>
+
+#include "rom_iap.h"
+#include "w25q80bv.h"
+
+#define ROM_IAP_ADDR (0x10400100)
+#define ROM_IAP_UNDEF_ADDR (0x12345678)
+
+#define ROM_OTP_PART_ID_ADDR (0x40045000)
+
+
+typedef void (* IAP_t)(uint32_t [],uint32_t[]);
+
+typedef	struct {
+   const IAP_t IAP;	/* If equal to 0x12345678 IAP not implemented */
+   /* Other TBD */
+} *pENTRY_ROM_API_t; 
+#define pROM_API ((pENTRY_ROM_API_t)ROM_IAP_ADDR)
+
+/* 
+ See Errata sheet ES_LPC43X0_A.pdf (LPC4350/30/20/10 Rev A)
+ 3.5 IAP.1: In-Application Programming API not present on flashless parts
+ Introduction:
+ The LPC43x0 microcontrollers contain an APIfor In-Application Programming of flash 
+ memory. This API also allows identification of the part.
+ Problem:
+ On the LPC43x0 microcontrollers, the IAP API is not present. The ISP interface is present 
+ which allows the part to be identified externally (via the UART) but part identification is not 
+ possible internally using the IAP call because it is not implemented.
+ The first word of the Part ID can be read directly from OTP at 0x40045000. The second word of the Part ID is always 
+'0' on flashless parts. */
+
+bool iap_is_implemented(void)
+{
+	bool res;
+	if( *((uint32_t*)ROM_IAP_ADDR) != ROM_IAP_UNDEF_ADDR )
+	{
+		res = true;
+	}else
+	{
+		res = false;
+	}
+	return res;
+}
+
+isp_iap_ret_code_t iap_cmd_call(iap_cmd_res_t* iap_cmd_res) 
+{
+	uint32_t* p_u32_data;
+	
+	if( iap_is_implemented() )
+	{
+		pROM_API->IAP( (uint32_t*)&iap_cmd_res->cmd_param, (uint32_t*)&iap_cmd_res->status_res);
+	}else
+	{
+		switch(iap_cmd_res->cmd_param.command_code)
+		{
+			/* Alternative way to retrieve Part Id on MCU with no IAP */
+			case IAP_CMD_READ_PART_ID_NO:
+				p_u32_data = (uint32_t*)ROM_OTP_PART_ID_ADDR;
+				iap_cmd_res->status_res.iap_result[0] = p_u32_data[0];
+				iap_cmd_res->status_res.iap_result[1] = p_u32_data[1];
+				iap_cmd_res->status_res.status_ret = CMD_SUCCESS;
+			break;
+			
+			/* Read Serial No => Read Unique ID in SPIFI (only compatible with W25Q80BV */
+			case IAP_CMD_READ_SERIAL_NO:
+			/* Only 64bits used */
+			spi_bus_start(spi_flash.bus, &ssp_config_w25q80bv);
+			w25q80bv_setup(&spi_flash);
+			iap_cmd_res->status_res.iap_result[0] = 0;
+			iap_cmd_res->status_res.iap_result[1] = 0;
+			w25q80bv_get_unique_id(&spi_flash, (w25q80bv_unique_id_t*)&iap_cmd_res->status_res.iap_result[2] );
+				iap_cmd_res->status_res.status_ret = CMD_SUCCESS;
+			break;
+			
+			default:
+				iap_cmd_res->status_res.status_ret = ERROR_IAP_NOT_IMPLEMENTED;
+			break;
+		}
+	}
+	return iap_cmd_res->status_res.status_ret;
+}
+
+#define HACKRF_VID (0x1d50)
+#define HACKRF_PID (0x6089)
+
+#define OTP_API_TABLE_OFFSET    0x1
+
+void write_USB_vid_pid(void)
+{
+	unsigned long *OTP_API_TABLE;
+	uint32_t (*otp_init)(void);
+	uint32_t (*otp_set_usb)(uint32_t ProductID, uint32_t VendorID);
+
+	OTP_API_TABLE = *((unsigned long * *) ROM_IAP_ADDR + OTP_API_TABLE_OFFSET);
+
+	otp_init = (uint32_t (*)(void))OTP_API_TABLE[0];
+	otp_init();
+
+	otp_set_usb = (uint32_t (*)(uint32_t pid, uint32_t vid))OTP_API_TABLE[3];
+	otp_set_usb(HACKRF_PID, HACKRF_VID);
+}
